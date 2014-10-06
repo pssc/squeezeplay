f3f10868e23fe9d50675c6de79511a5ea93cc018
pssc <phillip.camp+git@gmail.com>
2014-10-06 16:38:19 +0100

    Add Pi board/revision handling and scaling

--- src/squeezeplay_desktop/share/applets/SqueezePi/SqueezePiMeta.lua
+++ src/squeezeplay_desktop/share/applets/SqueezePi/SqueezePiMeta.lua
@@ -14,11 +14,14 @@ local System        = require("jive.System")
 local Player        = require("jive.slim.Player")
 local SlimServer        = require("jive.slim.SlimServer")
 local Decode        = require("squeezeplay.decode")
+local debug           = require("jive.utils.debug")
+local ArtworkCache = require("jive.slim.ArtworkCache")
 
 local appletManager = appletManager
 local jiveMain      = jiveMain
 local jive          = jive
 local jnt           = jnt
+local tonumber      = tonumber
 
 
 
@@ -57,28 +60,141 @@ function registerApplet(meta)
         for line in f:lines() do
             if string.match(line, "Hardware%s+: BCM2708") then
                pi = true
-            end
+           elseif string.match(line, "Revision%s+: %x+") then
+		local pi = { model="error", version=0, memory=0, revision=0, maker="", ov=false }
+               local nrev = string.match(line, "Revision%s+: (%x+)")
+		log:debug("Raw Revision: ", nrev)
+                nrev = tonumber(nrev,16)
+               if (not settings['Revision'] and nrev ) or (nrev and settings['Revision'] and settings['Revision'] != nrev) then
+                  settings['Revision']=nrev
+	          -- olde style overvolting - that will add in  1000000
+                  if ( nrev > 1000000 ) then
+			pi.ov = true
+			nrev = nrev - 1000000
+                  end
+                  pi.revision=nrev
+
+               if rev == 0 or nrev == 1 or nrev > 0x11 then
+		   log:error("Error Determining model of Pi")
+		   pi.model="error"
+		   pi = false
+               elseif nrev == 0x02 then
+                   pi.model = "B"
+                   pi.version = 1
+                   pi.memory = 256
+                   pi.maker = "Egoman"
+               elseif nrev == 0x03 then
+                   pi.model = "B"
+                   pi.version = 1.1
+                   pi.memory = 256
+                   pi.maker = "Egomanx"
+               elseif nrev == 0x04 then
+                   pi.model = "B"
+                   pi.version = 2
+                   pi.memory = 256
+                   pi.maker = "Sony"
+               elseif nrev == 0x05 then
+                   pi.model = "B"
+                   pi.version = 1.1
+                   pi.memory = 256
+                   pi.maker = "Qisda"
+               elseif nrev == 0x06 then
+                   pi.model = "B"
+                   pi.version = 2
+                   pi.memory = 256
+                   pi.maker = "Egoman"
+               elseif nrev == 0x07 then
+                   pi.model = "A"
+                   pi.version = 2
+                   pi.memory = 256
+                   pi.maker = "Egoman"
+               elseif nrev == 0x08 then
+                   pi.model = "A"
+                   pi.version = 2
+                   pi.memory = 256
+                   pi.maker = "Sony"
+               elseif nrev == 0x09 then
+                   pi.model = "A"
+                   pi.version = 2
+                   pi.memory = 256
+                   pi.maker = "Qisda"
+               elseif nrev == 0x0d then
+                   pi.model = "B"
+                   pi.version = 2
+                   pi.memory = 512
+                   pi.maker = "Egoman"
+               elseif nrev == 0x0e then
+                   pi.model = "B"
+                   pi.version = 2
+                   pi.memory = 512
+                   pi.maker = "Sony"
+               elseif nrev == 0x0f then
+                   pi.model = "B"
+                   pi.version = 2
+                   pi.memory = 256
+                   pi.maker = "Qisda"
+               elseif nrev == 0x10 then
+                   pi.model = "C"
+                   pi.version = 1.2
+                   pi.memory = 512
+                   pi.maker = "Sony"
+               elseif nrev == 0x11 then
+                   pi.model = "B+"
+                   pi.version = 1.2
+                   pi.memory = 512
+                   pi.maker = "Sony"
+		end
+
+               settings['pi'] = pi
+               -- save...
+               -- reset? settings.
+               end
+           end
         end
         f:close()
+
         if not pi then
            return
         end
+        log:info("Revision: ", string.format("%04x",settings['Revision']))
+        log:info("Pi: ", debug.view(settings['pi']))
+
+        -- sound... for pass through...
+	f = io.open("/usr/share/alsa/cards/bcm2835.conf")
+	if not f then
+		local pcms,err = io.popen("cp ../share/jive/applets/SqueezePi/bcm2835.conf /usr/share/alsa/cards/bcm2835.conf", "r")
+		if err then
+			log:error("Copy of sound card config failed ",err,".")
+		end
+	else
+		f:close()
+	end
+
+	-- appletManager:inhibitApplet("DesktopJive")
 
 	-- Set player device type 
-        LocalPlayer:setDeviceType("squeezeplay", "SqueezePi")
+	LocalPlayer:setDeviceType("squeezeplay", "SqueezePi")
 
-        -- Set the minimum support server version the version has to be greater than this ie 7.7+
-        SlimServer:setMinimumVersion("7.6")
+	-- Set the minimum support server version the version has to be greater than this ie 7.7+
+	SlimServer:setMinimumVersion("7.6")
 
-        jiveMain:addItem(meta:menuItem('piaudio_selector', 'settingsAudio', "AUDIO_SELECT", function(applet, ...) applet:settingsAudioSelect(...) end))
+	jiveMain:addItem(meta:menuItem('piaudio_selector', 'settingsAudio', "AUDIO_SELECT", function(applet, ...) applet:settingsAudioSelect(...) end))
 
-        if jiveMain:getDefaultSkin() == 'QVGAportraitSkin' then
-           jiveMain:setDefaultSkin("800x600Skin") -- FIXME 640x480... LCD
-        end
+	if jiveMain:getDefaultSkin() == 'QVGAportraitSkin' then -- Lowest common denominator
+		jiveMain:setDefaultSkin("800x600Skin") -- FIXME Resolution Detection....
+	end
 
-         -- open audio device
-        Decode:open(settings)
+	appletManager:addDefaultSetting("Playback", "enableAudio", 1)
 
+        -- sp default 8MB -- FIXME make tunebale this is per slimserver... FIXME...
+	if (settings['pi']['memory'] <= 256) then
+		ArtworkCache.setDefaultLimit(12*1024*1024) -- 12MB
+	else
+		ArtworkCache.setDefaultLimit(32*1024*1024) -- 32MB
+	end
+
+	-- open audio device
+	Decode:open(settings)
 end
 
 
