07d6e39fe41fe13a9699376613b87c4fbc5c4ad1
pssc <phillip.camp+git@gmail.com>
2015-04-07 11:07:08 +0100

    Add tests for requied tools and for reuired args with basic help #41

--- src/squeezeplay_desktop/squeezeplay-tunnel.sh
+++ src/squeezeplay_desktop/squeezeplay-tunnel.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-# test for connect proxy and pactester FIXME
+NF=""
+pactester -v >/dev/null 2>&1 || NF="$NF pactester"
+autossh -v   >/dev/null 2>&1 || NF="$NF autossh"
+connect-proxy -n >/dev/null 2>&1 || NF="$NF connect-proxy"
+curl --version >/dev/null 2>&1 || NF="$NF curl"
+if [ -n "$NF" ];then
+	echo "did not find$NF"
+	exit 1
+fi
+
+if [ -z "$1" ];then
+	echo "$0	TUNNEL_HOST [PORT] [DIRECT_PORT]"
+	exit 1
+fi
 
 SCRIPT=$0
 if [ "$1" = '-P' ];then
@@ -17,7 +30,7 @@ else
 	AUTOSSH_LOGLEVEL=5
 	export AUTOSSH_POLL AUTOSSH_LOGLEVEL
 	exec autossh -M 2000:7 -o "$SSHPROXY" -L 10000:localhost:9000 -L 4483:localhost:3483 -L 8888:localhost:8888 -o "ExitOnForwardFailure yes" -p ${PORT} -N squeezeplay@${HOST}
-	# Ideally we would have a tunnel per function but we would need to westart independanly
+	# Ideally we would have a tunnel per function but we would need to reestart independantly
 	#autossh -M 2000:7 -o "$SSHPROXY" -L 10000:localhost:9000 -o "ExitOnForwardFailure yes" -p ${PORT} -N squeezeplay@${HOST} &
 	#autossh -M 2001:7 -o "$SSHPROXY" -L 4483:localhost:3483 -o "ExitOnForwardFailure yes" -p ${PORT} -N squeezeplay@${HOST} &
 	#autossh -M 2002:7 -o "$SSHPROXY" -L 8888:localhost:8888 -o "ExitOnForwardFailure yes" -p ${PORT} -N squeezeplay@${HOST} &
