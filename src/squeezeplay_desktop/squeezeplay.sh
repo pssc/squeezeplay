1a082710a0adb7df023129eb1812dba77008a5d1
pssc <phillip.camp+git@gmail.com>
2015-06-15 17:50:51 +0100

    Script example support for RT #69, FPS close #33 and JIVE_WATCHDOG_FILE #79

--- src/squeezeplay_desktop/squeezeplay.sh
+++ src/squeezeplay_desktop/squeezeplay.sh
@@ -19,5 +19,21 @@ LD_LIBRARY_PATH=$LIB_DIR:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 PATH=$PATH:${SP_INSTALL_DIR}/bin
 export PATH
+JIVE_FR=${JIVE_FR:-14}
+export JIVE_FR
 
-cd ${SP_INSTALL_DIR}/bin && exec ./jive
+##
+# alsa threads are given an audio based RT PRi by the backend code
+# but for audio to be streamed we need the data provided from the jive
+# stream... so elevate above the rest of the system and mark as RT.
+# A custom script to get all the depenacies will be needed on Maxed out systems
+CHRT=""
+chrt --version  >/dev/null 2>&1 || NF="$NF chrt"
+if [ -n "$NF" ];then
+        echo "did not find$NF" >&2
+	CHRT="nice -n -1"
+else
+	[ -n "${JIVE_RT_PRI}" ] && CHRT="chrt -r ${JIVE_RT_PRI}"
+fi
+#: >${JIVE_WATCHDOG_FILE:-/tmp/jive}
+cd ${SP_INSTALL_DIR}/bin && exec ${CHRT} ./jive
