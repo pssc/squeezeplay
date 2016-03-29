b7136e484b02380269b234a30b1de9198d2cbd6d
pssc <phillip.camp+git@gmail.com>
2016-03-29 23:54:27 +0100

    RT hacking was a bad idea

--- src/squeezeplay/share/applets/Linux/LinuxMeta.lua
+++ src/squeezeplay/share/applets/Linux/LinuxMeta.lua
@@ -36,23 +36,5 @@ function jiveVersion(meta)
         return 1, 1
 end
 
--- RT script on post OnScreenInit
-function rt(self,name)
-        --if (string.match(os.getenv("JIVE_RT_PRI") or "", "([0-9]+)")) then
-                local script = name or "squeezeplay-rt.sh"
-                log:debug("RT script ",script," scheduled")
-                jiveMain:registerPostOnScreenInit(function()
-                        log:info("RT init ",script)
-                        local s,err = io.popen(script, "r")
-
-                        if err then
-                                log:error("RT ",script," could not be run ",err,".")
-                        else
-                                s:close()
-                        end
-                end)
-        --end
-end
-
 function registerApplet(meta)
 end
