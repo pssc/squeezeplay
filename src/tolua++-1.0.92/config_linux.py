42bce2e6c842285b19146c33c271dc25b27a169a
pssc <phillip.camp+git@gmail.com>
2016-03-30 16:10:48 +0100

    Fix to use build prefix

--- src/tolua++-1.0.92/config_linux.py
+++ src/tolua++-1.0.92/config_linux.py
@@ -2,9 +2,12 @@
 ## This is the linux configuration file
 # use 'scons -h' to see the list of command line options available
 
+import os
+prefix = os.getenv("PREFIX")
+
 # Compiler flags (based on Debian's installation of lua)
 #LINKFLAGS = ['-g']
-CCFLAGS = ['-I/usr/local/include', '-O2', '-ansi', '-Wall']
+CCFLAGS = ['-I/usr/local/include','-I'+prefix+'/include', '-O2', '-ansi', '-Wall']
 #CCFLAGS = ['-I/usr/include/lua50', '-g']
 
 # this is the default directory for installation. Files will be installed on
@@ -15,9 +18,7 @@ CCFLAGS = ['-I/usr/local/include', '-O2', '-ansi', '-Wall']
 #
 # You can see more 'generic' options for POSIX systems on config_posix.py
 
-prefix = '/usr/local'
-
 # libraries (based on Debian's installation of lua)
-LIBPATH = ['/usr/local/lib']
+LIBPATH = [prefix+'/lib','/usr/local/lib',prefix+'/lib']
 LIBS = ['lua', 'dl', 'm']
 
